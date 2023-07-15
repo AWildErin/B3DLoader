0b353cd9df1ec8c588edef075e9a839505e8eb13
AWildErin <sharpian126@gmail.com>
2023-07-15 13:41:54 +0100

    Add numeric extensions

---
+++ B3DLoader/Extensions/NumericExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B3DLoader.Extensions;
+
+public static class NumericExtensions
+{
+	public static float ToRadians( this float v )
+	{
+		return (float)(v * (Math.PI / 180f));
+	}
+
+	public static float ToDegrees( this float v )
+	{
+		return (float)(v * (180f / Math.PI));
+	}
+}
